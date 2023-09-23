3642ec0c9eabb21d949d2a7f855f69affa9a093c
Andrija Stevanović <andrijastev4@gmail.com>
2023-09-23 19:21:59 +0200

    moj rad hvala

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public float drillSpeed = 1f;
+    public float laziness = 0.1f;
+
+    private bool started = false;
+    private float actualSpeed = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            if (actualSpeed < drillSpeed)
+                actualSpeed += laziness;
+        }
+        else
+        {
+            if (actualSpeed > 0f)
+                actualSpeed -= laziness;
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y - (actualSpeed * Time.deltaTime), transform.position.z);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public Camera cam;
+    public GameObject sprite;
+
+    public float velocity = 1f;
+    public float laziness = 0.1f;
+    public float rotationScale = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = Vector2.MoveTowards(rb.velocity, new Vector2(Movement() * velocity, 0f), laziness);
+
+        sprite.transform.rotation = Quaternion.Euler(0, 0, rotationScale * Vector2.SignedAngle(Vector2.down, new Vector2(rb.velocity.x, -1f)));
+
+        Debug.Log(Vector2.down);
+        Debug.Log(new Vector2(rb.velocity.x, -1f));
+        Debug.Log(Vector2.SignedAngle(Vector2.down, new Vector2(rb.velocity.x, -1f)));
+
     }
+
+    private int Movement()  //-1 = left, 1 = right
+    {
+        //TODO: add touch input
+
+        if(Input.GetMouseButton(0))
+        {
+            
+            float xClick = cam.ScreenToWorldPoint(Input.mousePosition).x;
+            float xDrill = transform.position.x;
+            if (Mathf.Abs(xClick - xDrill) > 0.1f)
+            {
+                if (xClick > xDrill)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+
+        return 0;
+    }
+
 }
