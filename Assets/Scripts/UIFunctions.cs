548076528b534db335def29d5990557ba96fdb02
AsterixMitic <aca.mitic003@gmail.com>
2023-09-24 02:56:54 +0200

    UI

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_Functions : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject mainText;
+    public float moveSpeed = 10f;
+
+    public void openHidePanel()
+    {
+
+        var rectTrans = panel.GetComponent<RectTransform>();
+        if(rectTrans.anchoredPosition.y > 0)
+        {
+            rectTrans.anchoredPosition = new Vector2(rectTrans.anchoredPosition.x, -220f);
+        }
+        else
+        {
+            rectTrans.anchoredPosition = new Vector2(rectTrans.anchoredPosition.x, 400f);
+        }
+
+
+        if (mainText != null)
+        {
+            bool isTextActive = mainText.activeSelf;
+
+            mainText.SetActive(!isTextActive);
+        }
+           
+    }
+
+}
